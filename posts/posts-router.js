a04c02191194cdb065a1f88be428f3b8bf05f9db
Angel Torres <alas4224@gmail.com>
2019-02-13 17:21:56 -0600

    finished adding all CRUD request handles to posts and users routes

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -33,4 +33,37 @@ routerPosts.post('/', (req, res) => {
     }
 })
 
+routerPosts.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    if(!req.body.text | !req.body.user_id) {
+        res.status(400).json({errorMessage:"Please provide text and user id for the post."})
+    } else {
+        try {
+            const updatedPost = await Posts.update(id, changes);
+            if (updatedPost) {
+                res.status(200).json(updatedPost);
+            } else {
+                res.status(404).json({errorMessage:"Post could not be found."});
+            }
+        } catch {
+            res.status(500).json({errorMessage:"Could not update post"})
+        }
+    }
+})
+
+routerPosts.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const deleted = await Posts.remove(id);
+        if(!deleted) {
+            res.status(400).json({errorMessage:"Post could not be found."})
+        } else {
+            res.status(200).json(deleted)
+        }
+    } catch {
+        res.status(500).json({errorMessage:"Could not update post"});
+    }
+})
+
 module.exports = routerPosts;
--- users/users-router.js
+++ users/users-router.js
@@ -48,4 +48,37 @@ routerUsers.post('/', (req, res) => {
     }
 })
 
+routerUsers.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    if(!changes.name) {
+        res.status(400).json({errorMessage:"Please provide name for user"})
+    } else {
+        try {
+            const updatedUser = await Users.update(id, changes);
+            if (updatedUser) {
+                res.status(200).json(updatedUser);
+            } else {
+                res.status(404).json({errorMessage:"User could not be found."});
+            }
+        } catch {
+            res.status(500).json({errorMessage:"Could not update user"});
+        }
+    }
+});
+
+routerUsers.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const deleted = await Users.remove(id);
+        if(!deleted) {
+            res.status(400).json({errorMessage:"User could not be found."})
+        } else {
+            res.status(200).json(deleted)
+        }
+    } catch {
+        res.status(500).json({errorMessage:"Could not update user"});
+    }
+})
+
 module.exports = routerUsers;
